257682af9bc06d86502c8735090aeaee6358b7b9
Sam Webster <sw3bst3r@gmail.com>
2023-03-09 15:59:05 -0500

    Add scratch vpc w/ igw and route table

--- terraform-learning/terraform/main.tf
+++ terraform-learning/terraform/main.tf
@@ -2,40 +2,57 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "cidr-blocks" {
-  description = "cidr blocks for vpc and subnets"
-  type = list(object({
-    cidr-block = string
-    name = string
-  }))
+variable "vpc_cidr_block" {
 }
-# variable "vpc-cidr-block" {
-#   description = "vpc cidr block"
-# }
+variable "subnet_cidr_block" {
+}
+variable "availability_zone" {
+}
+variable "env_prefix" {
+}
 variable "environment" {
   description = "deployment environment"
 }
 
-resource "aws_vpc" "development-vpc" {
-  cidr_block = var.cidr-blocks[0].cidr-block
+resource "aws_vpc" "application-vpc" {
+  cidr_block = var.vpc_cidr_block
   tags = {
-    Name : var.cidr-blocks[0].name,
+    Name : "${var.env_prefix}-vpc"
     ENV : var.environment
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id            = aws_vpc.development-vpc.id
-  cidr_block        = var.cidr-blocks[1].cidr-block
-  availability_zone = "us-east-1a"
+resource "aws_subnet" "application-subnet-1" {
+  vpc_id            = aws_vpc.application-vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.availability_zone
   tags = {
-    Name : var.cidr-blocks[1].name
+    Name : "${var.env_prefix}-subnet-1"
   }
 }
 
-output "dev-vpc-id" {
-  value = aws_vpc.development-vpc.id
+
+resource "aws_internet_gateway" "application-internet-gateway" {
+  vpc_id = aws_vpc.application-vpc.id
+  tags = {
+    Name : "${var.env_prefix}-igw"
+  }
 }
-output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
+
+resource "aws_default_route_table" "application-default-rtb" {
+  default_route_table_id = aws_vpc.application-vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.application-internet-gateway.id
+  }
+  tags = {
+    Name : "${var.env_prefix}-main-rtb"
+  }
+}
+
+output "vpc-id" {
+  value = aws_vpc.application-vpc.id
+}
+output "subnet-id" {
+  value = aws_subnet.application-subnet-1.id
 }
--- terraform-learning/terraform/terraform-dev.tfvars
+++ terraform-learning/terraform/terraform-dev.tfvars
@@ -1,6 +1,6 @@
-cidr-blocks = [
-    {cidr-block="10.0.0.0/16",name="dev-vpc"},
-    {cidr-block="10.0.10.0/24",name="dev-subnet-1"}
-]
 #vpc-cidr-block    = "10.0.0.0/16"
 environment       = "dev"
+env_prefix        = "dev"
+vpc_cidr_block    = "10.0.0.0/16"
+subnet_cidr_block = "10.0.10.0/24"
+availability_zone = "us-east-1a"
